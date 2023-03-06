de02ea537cb56cf46c60976ae7c9509ee725d26e
NienDev <niewebdev@gmail.com>
2023-03-06 19:55:45 +0700

    create a layout for driver

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "usb-drive.png");
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(180, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(191, 97);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(113, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            this.label2.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(112, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            this.label1.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(86, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+        private Label new_label;
+        private Label[] drive_labels = new Label[100];
+        private ImageList imageList1;
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int originalPanelSize_width, originalPanelSize_height;
+        public Form1()
+        {
+            InitializeComponent();
+
+            originalPanelSize_width = panel1.Width;
+            originalPanelSize_height = panel1.Height;
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            for (int i =0; i < allDrives.Length; i++)
+            {
+                if (allDrives[i].DriveType.ToString() == "Removable")
+                {
+                    // create objects
+                    drive_labels[i] = new Label();
+
+                    // set properties
+                    drive_labels[i].Location = new Point(100, 100 *i);
+                    drive_labels[i].Name = "drive_label_" + i.ToString();
+                    drive_labels[i].Text = "Drive label " + i.ToString();
+                    
+                    // add to controls
+                    Controls.Add(drive_labels[i]);
+                }
+            }
+
+           
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+            if (imageList1.Images.Count > 0){
+                pictureBox1.Image = imageList1.Images[0];
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            panel1.Size = new Size(220, 120);
+            label1.Font= new Font("Microsoft Sans Serif", 18.25F, FontStyle.Bold);
+            label2.Font = new Font("Microsoft Sans Serif", 14.25F);
+            pictureBox1.Size = new Size(100, 90);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseLeave(object sender, EventArgs e)
+        {
+            panel1.Size = new Size(188, 100);
+            label1.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
+            label2.Font = new Font("Microsoft Sans Serif", 10.25F);
+            pictureBox1.Size = new Size(86, 77);
+        }
+
+    }
+}
