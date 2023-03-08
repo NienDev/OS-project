def4c10a0a1be53799f1868af25e60d0aabfd923
NienDev <niewebdev@gmail.com>
2023-03-08 10:27:37 +0700

    finish treeview

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -35,18 +35,18 @@ namespace WindowsFormsApp1
 
         
 
-        public int getNumberOfUSB()
-        {
-            int ans = 0;
-            for (int i = 0; i < allDrives.Length; i++)
-            {
-                if (allDrives[i].DriveType.ToString() == "Removable")
-                {
-                    ans++;
-                }
-            }
-            return ans;
-        }
+        //public int getNumberOfUSB()
+        //{
+        //    int ans = 0;
+        //    for (int i = 0; i < allDrives.Length; i++)
+        //    {
+        //        if (allDrives[i].DriveType.ToString() == "Removable")
+        //        {
+        //            ans++;
+        //        }
+        //    }
+        //    return ans;
+        //}
 
 
 
@@ -55,12 +55,8 @@ namespace WindowsFormsApp1
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
             // imageList1
@@ -69,70 +65,32 @@ namespace WindowsFormsApp1
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             this.imageList1.Images.SetKeyName(0, "usb-drive.png");
             // 
-            // panel1
+            // progressBar1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Location = new System.Drawing.Point(490, 166);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(298, 119);
-            this.panel1.TabIndex = 0;
+            this.progressBar1.Location = new System.Drawing.Point(562, 47);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(270, 10);
+            this.progressBar1.TabIndex = 3;
             // 
-            // label2
+            // treeView1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label2.Location = new System.Drawing.Point(169, 68);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 22);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(167, 33);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 29);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(115, 95);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.treeView1.Location = new System.Drawing.Point(562, 63);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(270, 302);
+            this.treeView1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.progressBar1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.form1_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -142,10 +100,6 @@ namespace WindowsFormsApp1
         private Label new_label;
         private Label[] drive_labels = new Label[100];
         private ImageList imageList1;
-        private Panel panel1;
-        private Label label2;
-        private Label label1;
-        private PictureBox pictureBox1;
         private Panel panel2;
 
         // Drive component include: 
@@ -153,13 +107,8 @@ namespace WindowsFormsApp1
         private Label[] drive_name_labels = new Label[100];
         private Label[] drive_format_labels = new Label[100];
         private PictureBox[] drive_image_pictureboxes = new PictureBox[100];
-
-        // all drivers
-        private DriveInfo[] allDrives = DriveInfo.GetDrives();
-
-        
-
-        
+        private ProgressBar progressBar1;
+        private TreeView treeView1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -18,6 +18,22 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
 
+        // all drivers
+        static DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        static int getNumberOfUSB()
+        {
+            int ans = 0;
+            for (int i = 0; i < allDrives.Length; i++)
+            {
+                if (allDrives[i].DriveType.ToString() == "Removable")
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
         public class DriveComponent
         {
             public Panel layout = new Panel();
@@ -36,7 +52,27 @@ namespace WindowsFormsApp1
             }
         }
 
-        public DriveComponent[] drives = new DriveComponent[100];
+        public DriveComponent[] drives = new DriveComponent[getNumberOfUSB()];
+
+        public void loadTreeView(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
+        {
+            TreeNode curNode = addInMe.Add(directoryInfo.Name);
+            try
+            {
+                foreach (FileInfo file in directoryInfo.GetFiles())
+                {
+                    curNode.Nodes.Add(file.FullName, file.Name);
+                }
+                foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
+                {
+                    loadTreeView(subdir, curNode.Nodes);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
 
         public Form1()
         {
@@ -52,13 +88,8 @@ namespace WindowsFormsApp1
                     usb_formats.Add(allDrives[i].DriveFormat);
                 }
             }
-            //foreach (string name in usb_names)
-            //{
-            //    MessageBox.Show(name);
-            //}
 
-
-                // create drive component
+            // create drive component
             for (int i = 0; i < allDrives.Length; i++)
             {
                 if (allDrives[i].DriveType.ToString() == "Removable")
@@ -77,119 +108,6 @@ namespace WindowsFormsApp1
                 drives[i] = new DriveComponent();
             }
 
-
-            
-
-
-            // set properties
-            //for (int i = 0; i < allDrives.Length; i++)
-            //{
-            //    if (allDrives[i].DriveType.ToString() == "Removable")
-            //    {
-                  
-            //        // layout
-            //        drive_layouts_panels[i].BackColor = SystemColors.Control;
-            //        drive_layouts_panels[i].BorderStyle = BorderStyle.FixedSingle;
-            //        drive_layouts_panels[i].Controls.Add(drive_name_labels[i]);
-            //        drive_layouts_panels[i].Controls.Add(drive_format_labels[i]);
-            //        drive_layouts_panels[i].Controls.Add(drive_image_pictureboxes[i]);
-            //        drive_layouts_panels[i].Cursor = Cursors.Hand;
-            //        drive_layouts_panels[i].ForeColor = SystemColors.ActiveCaptionText;
-            //        drive_layouts_panels[i].Location = new Point(10, 10 * i * 10);
-            //        drive_layouts_panels[i].Name = "drive_layouts_panels_" + i.ToString();
-            //        drive_layouts_panels[i].Size = new Size(191, 97);
-            //        //drive_layouts_panels[i].MouseEnter += (sender, e) => driveLayout_MouseEnter(allDrives[i].Name);
-            //        //drive_layouts_panels[i].MouseEnter += new EventHandler(this.driveLayout_MouseEnter);
-            //        //drive_layouts_panels[i].MouseLeave += new EventHandler(this.driveLayout_MouseLeave);
-
-
-            //        // name
-            //        drive_name_labels[i].AutoSize = true;
-            //        drive_name_labels[i].Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
-            //        drive_name_labels[i].Location = new Point(112, 24);
-            //        drive_name_labels[i].Name = "drive_name_label_" + i.ToString();
-            //        drive_name_labels[i].Size = new Size(66, 24);
-            //        drive_name_labels[i].Text = allDrives[i].Name;
-            //        //drive_name_labels[i].MouseEnter += new EventHandler(this.driveName_MouseEnter);
-            //        //drive_name_labels[i].MouseLeave += new EventHandler(this.driveName_MouseLeave);
-
-            //        // format
-            //        drive_format_labels[i].AutoSize = true;
-            //        drive_format_labels[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            //        drive_format_labels[i].Location = new System.Drawing.Point(113, 53);
-            //        drive_format_labels[i].Name = "drive_format_label_" + i.ToString();
-            //        drive_format_labels[i].Size = new System.Drawing.Size(46, 17);
-            //        drive_format_labels[i].Text = this.allDrives[i].DriveFormat;
-            //        //drive_name_labels[i].MouseEnter += new EventHandler(this.driveFormat_MouseEnter);
-            //        //drive_name_labels[i].MouseLeave += new EventHandler(this.driveFormat_MouseLeave);
-
-            //        // image
-            //        drive_image_pictureboxes[i].InitialImage = null;
-            //        drive_image_pictureboxes[i].Location = new Point(20, 10);
-            //        drive_image_pictureboxes[i].Name = "drive_image_pictureboxes_" + i.ToString();
-            //        drive_image_pictureboxes[i].Size = new Size(86, 77);
-            //        drive_image_pictureboxes[i].SizeMode = PictureBoxSizeMode.Zoom;
-            //        drive_image_pictureboxes[i].TabStop = false;
-            //        //dive_image_pictureboxes[i].MouseEnter += new System.EventHandler(this.driveImage_MouseEnter);
-            //        //drive_image_pictureboxes[i].MouseLeave += new System.EventHandler(this.driveImage_MouseLeave);
-
-            //        // Hover effect
-            //        this.drive_image_pictureboxes[i].MouseEnter += (sender, e) =>
-            //        {
-                        
-            //            MessageBox.Show(i.ToString());
-            //            //PictureBox img = (PictureBox)sender;
-            //            //Panel panel = (Panel)img.Parent;
-            //            //drive_layouts_panels[i].Size = new Size(211, 117);
-
-            //            //string s = "";
-            //            //foreach (Control child in panel.Controls)
-            //            //{
-            //            //    switch (child.Name)
-            //            //    {
-            //            //        case "label1":
-            //            //            child.Font = new Font("Microsoft Sans Serif", 18.25F, FontStyle.Bold);
-            //            //            break;
-            //            //        case "label2":
-            //            //            child.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
-            //            //            break;
-            //            //        default:
-            //            //            child.Size = new Size(120, 110);
-            //            //            break;
-            //            //    }
-            //            //}
-            //        };
-
-            //        drive_image_pictureboxes[i].MouseLeave += (sender, e) =>
-            //        {
-
-            //            //PictureBox img = (PictureBox)sender;
-            //            //Panel panel = (Panel)img.Parent;
-            //            //drive_layouts_panels[i].Size = new Size(191, 97);
-
-            //            //foreach (Control child in panel.Controls)
-            //            //{
-            //            //    switch (child.Name)
-            //            //    {
-            //            //        case "label1":
-            //            //            child.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
-            //            //            break;
-            //            //        case "label2":
-            //            //            child.Font = new Font("Microsoft Sans Serif", 10.25F, FontStyle.Bold);
-            //            //            break;
-            //            //        default:
-            //            //            child.Size = new Size(100, 90);
-            //            //            break;
-            //            //    }
-            //            //}
-            //        };
-
-
-            //        this.Controls.Add(drive_layouts_panels[i]);
-            //    }
-            //}
-
-
             for (int i = 0; i < getNumberOfUSB(); i++)
             {
                 // layout
@@ -203,10 +121,8 @@ namespace WindowsFormsApp1
                 drives[i].layout.Location = new Point(10, 10 * i * 10);
                 drives[i].layout.Name = "drive_layouts_panels_" + i.ToString();
                 drives[i].layout.Size = new Size(191, 97);
-                //drive_layouts_panels[i].MouseEnter += (sender, e) => driveLayout_MouseEnter(allDrives[i].Name);
-                //drive_layouts_panels[i].MouseEnter += new EventHandler(this.driveLayout_MouseEnter);
-                //drive_layouts_panels[i].MouseLeave += new EventHandler(this.driveLayout_MouseLeave);
 
+      
 
                 // name
                 drives[i].name.AutoSize = true;
@@ -215,8 +131,8 @@ namespace WindowsFormsApp1
                 drives[i].name.Name = "drive_name_label_" + i.ToString();
                 drives[i].name.Size = new Size(66, 24);
                 drives[i].name.Text = usb_names[i];
-                //drive_name_labels[i].MouseEnter += new EventHandler(this.driveName_MouseEnter);
-                //drive_name_labels[i].MouseLeave += new EventHandler(this.driveName_MouseLeave);
+                
+
 
                 // format
                 drives[i].format.AutoSize = true;
@@ -225,8 +141,7 @@ namespace WindowsFormsApp1
                 drives[i].format.Name = "drive_format_label_" + i.ToString();
                 drives[i].format.Size = new System.Drawing.Size(46, 17);
                 drives[i].format.Text = usb_formats[i];
-                //drive_name_labels[i].MouseEnter += new EventHandler(this.driveFormat_MouseEnter);
-                //drive_name_labels[i].MouseLeave += new EventHandler(this.driveFormat_MouseLeave);
+          
 
                 // image
                 drives[i].image.InitialImage = null;
@@ -235,76 +150,37 @@ namespace WindowsFormsApp1
                 drives[i].image.Size = new Size(86, 77);
                 drives[i].image.SizeMode = PictureBoxSizeMode.Zoom;
                 drives[i].image.TabStop = false;
-                //dive_image_pictureboxes[i].MouseEnter += new System.EventHandler(this.driveImage_MouseEnter);
-                //drive_image_pictureboxes[i].MouseLeave += new System.EventHandler(this.driveImage_MouseLeave);
-
-                // Hover effect
-                //this.drive_image_pictureboxes[i].MouseEnter += (sender, e) =>
-                //{
-
-                    
-                    //PictureBox img = (PictureBox)sender;
-                    //Panel panel = (Panel)img.Parent;
-                    //drive_layouts_panels[i].Size = new Size(211, 117);
-
-                    //string s = "";
-                    //foreach (Control child in panel.Controls)
-                    //{
-                    //    switch (child.Name)
-                    //    {
-                    //        case "label1":
-                    //            child.Font = new Font("Microsoft Sans Serif", 18.25F, FontStyle.Bold);
-                    //            break;
-                    //        case "label2":
-                    //            child.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
-                    //            break;
-                    //        default:
-                    //            child.Size = new Size(120, 110);
-                    //            break;
-                    //    }
-                    //}
-                //};
-
-                //drive_image_pictureboxes[i].MouseLeave += (sender, e) =>
-                //{
-
-                    //PictureBox img = (PictureBox)sender;
-                    //Panel panel = (Panel)img.Parent;
-                    //drive_layouts_panels[i].Size = new Size(191, 97);
-
-                    //foreach (Control child in panel.Controls)
-                    //{
-                    //    switch (child.Name)
-                    //    {
-                    //        case "label1":
-                    //            child.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
-                    //            break;
-                    //        case "label2":
-                    //            child.Font = new Font("Microsoft Sans Serif", 10.25F, FontStyle.Bold);
-                    //            break;
-                    //        default:
-                    //            child.Size = new Size(100, 90);
-                    //            break;
-                    //    }
-                    //}
-                //};
-
+       
 
                 this.Controls.Add(drives[i].layout);
             }
 
+            // Add Hover Effect
             int index = 0;
             foreach(DriveComponent component in drives)
             {
                 if (component != null)
                 {
+                    component.image.Click += (s, e) => {
+                        foreach (DriveComponent component1 in drives)
+                        {
+                            // hide all components
+                            component1.layout.Hide();
+                            
+                        }
+                        DirectoryInfo directoryInfo = new DirectoryInfo(component.name.Text);
+                        if (directoryInfo.Exists)
+                        {
+                            loadTreeView(directoryInfo, treeView1.Nodes);
+                        }
+                    };
+                   
                     component.image.MouseEnter += (sender, e) =>
                     {
                         PictureBox img = (PictureBox)sender;
                         Panel panel = (Panel)img.Parent;
                         panel.Size = new Size(211, 117);
 
-                        string s = "";
                         foreach (Control child in panel.Controls)
                         {
                             if (child.Name.Contains("name"))
@@ -342,46 +218,18 @@ namespace WindowsFormsApp1
                             }
                         }
                     };
+
+                  
+                    
                 }
             }
-            
-
-            //DriveInfo[] allDrives = DriveInfo.GetDrives();
-
-            //for (int i =0; i < allDrives.Length; i++)
-            //{
-            //    if (allDrives[i].DriveType.ToString() == "Removable")
-            //    {
-            //        // create objects
-            //        drive_labels[i] = new Label();
-
-            //        // set properties
-            //        drive_labels[i].Location = new Point(100, 100 *i);
-            //        drive_labels[i].Name = "drive_label_" + i.ToString();
-            //        drive_labels[i].Text = "Drive label " + i.ToString();
-
-            //        // add to controls
-            //        Controls.Add(drive_labels[i]);
-            //    }
-            //}
-
-
+   
         }
 
         private void form1_Load(object sender, EventArgs e)
         {
             if (imageList1.Images.Count > 0)
             {
-                pictureBox1.Image = imageList1.Images[0];
-
-                //for (int i = 0; i < allDrives.Length; i++)
-                //{
-                //    if (allDrives[i].DriveType.ToString() == "Removable")
-                //    {
-                //        drive_image_pictureboxes[i].Image = imageList1.Images[0];
-                //    }
-                //}
-
                 for (int i = 0; i < getNumberOfUSB(); i++)
                 {
                     drives[i].image.Image = imageList1.Images[0];
@@ -402,29 +250,12 @@ namespace WindowsFormsApp1
         private void driveImage_MouseEnter(object sender, EventArgs e)
         {
 
-            //if (sender.GetType().ToString() == "Panel")
-            //{
-
-            //} else if (sender.GetType().ToString() == "Label")
-            //{
-
-            //} else
-            //{
-
-            //}
             MessageBox.Show("hover");
             PictureBox pictureBox = (PictureBox)sender;
-            //Label[] labels = (Label[])sender;
-            //PictureBox pictureBox = (PictureBox)sender;
-
-            //panel.Size = new Size(220, 120);
-            //labels[0].Font = new Font("Microsoft Sans Serif", 18.25F, FontStyle.Bold);
-            //labels[1].Font = new Font("Microsoft Sans Serif", 14.25F);
+        
             pictureBox.Size = new Size(100, 90);
 
-            //int index = pictureBox.Name[pictureBox.Name.Length - 1];
-
-            //driveName_MouseEnter((Label)drive_name_labels[index-1], EventArgs.Empty);
+     
 
         }
 
@@ -436,9 +267,7 @@ namespace WindowsFormsApp1
         private void driveImage_MouseLeave(object sender, EventArgs e)
         {
             PictureBox pictureBox = (PictureBox)sender;
-            //panel.Size = new Size(188, 100);
-            //label1.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold);
-            //label2.Font = new Font("Microsoft Sans Serif", 10.25F);
+
             pictureBox.Size = new Size(86, 77);
 
         }
@@ -467,16 +296,18 @@ namespace WindowsFormsApp1
         private void driveLayout_MouseEnter(object sender, EventArgs e)
         {
             MessageBox.Show("panel in");
-            //Panel panel = (Panel)sender;
-            //panel.Size = new Size(220, 120);
+       
         }
         private void driveLayout_MouseLeave(object sender, EventArgs e)
         {
             MessageBox.Show("panel out");
-            //Panel panel = (Panel)sender;
-            //panel.Size = new Size(188, 100);
+      
 
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
     }
 }
